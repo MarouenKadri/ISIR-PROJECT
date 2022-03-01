edd47543a06a1a3192ea4efbc95ae68c272ca23b
MarouenKadri <marouen.kadri.24@gmail.com>
2022-03-01 12:50:51 -0800

    tp1

---
+++ TP1/src/geometry/sphere_geometry.cpp
@@ -0,0 +1,30 @@
+#include "sphere_geometry.hpp"
+
+namespace RT_ISICG
+{
+	bool SphereGeometry::intersect( const Ray & p_ray, float & p_t1, float & p_t2 ) const
+	{
+		Vec3f oc	= p_ray.getOrigin() - _center;
+		float b		= 2.0f * dot( oc, p_ray.getDirection() );
+		float c		= dot( oc, oc ) - _radius * _radius;
+		float Delta = b * b - 4 * c;
+		if ( Delta < 0 ) return false;  
+		float s1 = ( -b - sqrt( Delta ) ) * 0.5f;
+		float s2 = ( -b + sqrt( Delta ) ) * 0.5f;  
+		if (s2 < s1)
+		{ p_t1 = s2;  
+		  p_t2 = s1;
+		}    
+		else  
+		{
+		  p_t2 = s1;  
+		  p_t1 = s2;
+		
+		}
+
+
+
+		return true;
+	}
+
+} // namespace RT_ISICG
---
+++ TP1/src/integrators/ray_cast_integrator.cpp
@@ -0,0 +1,22 @@
+#include "ray_cast_integrator.hpp"
+
+namespace RT_ISICG
+{
+	Vec3f RayCastIntegrator::Li( const Scene & p_scene,
+								 const Ray &   p_ray,
+								 const float   p_tMin,
+								 const float   p_tMax ) const
+	{
+		HitRecord hitRecord;
+		if ( p_scene.intersect( p_ray, p_tMin, p_tMax, hitRecord ) )
+		{
+			/// TODO ! cos theta...
+			return hitRecord._object->getMaterial()->getFlatColor()
+				   * std::max( 0.0f, glm::dot( -p_ray.getDirection(), hitRecord._normal ) );
+		}
+		else
+		{
+			return _backgroundColor;
+		}
+	}
+} // namespace RT_ISICG
